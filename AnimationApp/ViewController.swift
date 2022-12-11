b58b4ae1fc155c0b556a6bee355bf981764d367d
Valentin <vaminov94@gmail.com>
2022-12-11 17:49:28 +0300

    animation with Spring

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import SpringAnimation
 
 class ViewController: UIViewController {
-
+ //MARK: - IBOutlets
+    
+    @IBOutlet var labelView: UILabel!
+    @IBOutlet var animationView: SpringView!
+    @IBOutlet var animatiomButton: SpringButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        settingsView()
+    }
+    
+    @IBAction func startAnimationButton(_ sender: SpringButton) {
+        animation()
     }
 
-
+  // MARK: - ButtonStart
+    func animation() {
+        animationView.animation = "\(AnimationPreset.allCases.randomElement()!)"
+        animationView.curve = "\(AnimationCurve.allCases.randomElement()!)"
+        animationView.animate()
+        labelView.text = "\(AnimationPreset.allCases.randomElement()!)\n\(AnimationCurve.allCases.randomElement()!)"
+        
+    }
+    
+    //MARK: - SettingsStartView
+    func settingsView() {
+        animationView.layer.cornerRadius = 10
+        animatiomButton.layer.cornerRadius = 10
+        labelView.text = "Animation"
+    }
 }
-
